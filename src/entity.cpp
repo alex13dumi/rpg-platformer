58dcb8808c3f2a9933ef27c94159da633af4620b
alex13dumi <alex13.dumi@gmail.com>
2022-10-16 12:50:48 +0300

    Added getters for _texture and _window;

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,12 +1,14 @@
 #include <Entity.hpp>
+#include <Math.hpp>
 
-Entity::Entity(Vector2f pos, SDL_Texture* texture): _pos(pos), _texture(texture) {
+Entity::Entity(Vector2f pos, SDL_Texture* texture, Vector2f resize): _pos(pos), _texture(texture), _resize(resize) {
     _currentFrame.x = 0;
     _currentFrame.y = 0;
     _currentFrame.w = 32;
     _currentFrame.h = 32;
 }
 
-SDL_Texture* Entity::getText() { return _texture; };
-SDL_Rect Entity::getCurrentFrame() { return _currentFrame; }
 Vector2f& Entity::getPos() { return _pos; }
+SDL_Texture* Entity::getText() { return _texture; }
+SDL_Rect Entity::getCurrentFrame() { return _currentFrame; }
+Vector2f& Entity::getResize(){ return _resize; }
