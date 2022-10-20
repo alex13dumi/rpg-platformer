cda57c75962907eb1e15111cf329c694a3375fb4
alex13dumi <alex13.dumi@gmail.com>
2022-10-20 21:00:33 +0300

    contains Constructor definition for Sprites.hpp

--- src/sprites.cpp
+++ src/sprites.cpp
@@ -1,4 +1,3 @@
 #include <Sprites.hpp>
 
-Hero::Hero(std::size_t row, std::size_t col, std::size_t widht, std::size_t height): _col(col), _row(row), _width(widht), _height(height) {};
-
+Hero::Hero(std::size_t row, std::size_t col, std::size_t widht, std::size_t height): _col(col), _row(row), _width(widht), _height(height){ };
