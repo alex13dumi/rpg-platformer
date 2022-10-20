f34a8b146f94d6d8a95e8e5978907f642fe8e212
alex13dumi <alex13.dumi@gmail.com>
2022-10-20 21:01:08 +0300

    latest version of main; code more compacted; to do, get rid of for;

--- src/test.cpp
+++ src/test.cpp
@@ -7,6 +7,7 @@
 #include <RenderWindow.hpp>
 #include <Entity.hpp>
 #include <System.hpp>
+#include <Sprites.hpp>
 
 #define WIDTH 1280
 #define HEIGHT 720
@@ -28,7 +29,7 @@ int main(int argc, char* args[])
 
     /*Load textures*/
     SDL_Texture* grassTexture = window.loadTexture("../res/gfx/grass.png");
-    SDL_Texture* hero = window.loadTexture("../res/gfx/adventurer-sheet.png");
+    SDL_Texture* heroTexture = window.loadTexture("../res/gfx/adventurer-sheet.png");
 
     std::vector<Entity> entitiees = {Entity( Vector2f(0, 120), grassTexture, Vector2f(8,8)),
                                      Entity( Vector2f(64, 120), grassTexture, Vector2f(8,8)),
@@ -39,35 +40,14 @@ int main(int argc, char* args[])
 
     std::vector<SDL_Rect*> rects;
     /* Each sprite is an 50 x 37 images. TO DO: Store them in a JSON LUA data file*/
-    size_t nbRow = 11;
-    size_t nbCol = 7;
-    size_t widthSpr = 50;
-    size_t heightSpr = 37;
+    Hero hero(11, 7, 50, 37);
 
-    for (size_t i = 0; i < nbRow; i++) {
-        for (size_t j = 0; j < nbCol; j++) {
-            rects.push_back(new SDL_Rect{ (int) (j*widthSpr), (int) (i * heightSpr), (int) widthSpr, (int) heightSpr });
+    for (std::size_t i = 0; i < hero._row; i++) {
+        for (std::size_t j = 0; j < hero._col; j++) {
+            rects.push_back(new SDL_Rect{ (int) (j*hero._width), (int) (i * hero._height), (int) hero._width, (int) hero._height });
         }
     }
-
-    std::vector<std::pair<size_t, size_t>> idle1  { {0, 0}, {0, 1}, {0, 2}, {0, 3} };
-    std::vector<std::pair<size_t, size_t>> crouch { {0, 4}, {0, 5}, {0, 6}, {1, 0} };
-    std::vector<std::pair<size_t, size_t>> run    { {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6} };
-    std::vector<std::pair<size_t, size_t>> jump   { {2, 0}, {2, 1}, {2, 2}, {2, 3} };
-    std::vector<std::pair<size_t, size_t>> mid    { {2, 4}, {2, 5}, {2, 6}, {3, 0} };
-    std::vector<std::pair<size_t, size_t>> fall   { {3, 1}, {3, 2} };
-    std::vector<std::pair<size_t, size_t>> slide  { {3, 3}, {3, 4}, {3, 5}, {3, 6}, {4, 0} };
-    std::vector<std::pair<size_t, size_t>> grab   { {4, 1}, {4, 2}, {4, 3}, {4, 4}};
-    std::vector<std::pair<size_t, size_t>> climb  { {4, 5}, {4, 6}, {5, 0}, {5, 1}, {5, 2} };
-    std::vector<std::pair<size_t, size_t>> idle2  { {5, 3}, {5, 4}, {5, 5}, {5, 6} };
-    std::vector<std::pair<size_t, size_t>> attack1{ {6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4} };
-    std::vector<std::pair<size_t, size_t>> attack2{ {6, 5}, {6, 6}, {7, 0}, {7, 1}, {7, 2}, {7, 3} };
-    std::vector<std::pair<size_t, size_t>> attack3{ {7, 4}, {7, 5}, {7, 6}, {8, 0}, {8, 1}, {8, 2} };
-    std::vector<std::pair<size_t, size_t>> hurt   { {8, 3}, {8, 4}, {8, 5} };
-    std::vector<std::pair<size_t, size_t>> die    { {8, 6}, {9, 0}, {9, 1}, {9, 2}, {9, 3}, {9, 4}, {9, 5} };
-    std::vector<std::pair<size_t, size_t>> jump2  { {9, 6}, {10, 0}, {10, 1} };
-
-    std::vector<std::pair<size_t, size_t>> current = idle1;
+    Vector current = hero.idle1;
     size_t index = 0;
 
     double maxDuration = 150;
@@ -91,52 +71,52 @@ int main(int argc, char* args[])
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     gameRunning = true;
                 if (event.key.keysym.scancode == SDL_SCANCODE_Q) {
-                    current = idle1;
+                    current = hero.idle1;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_W) {
-                    current = crouch;
+                    current = hero.crouch;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_E) {
-                    current = run;
+                    current = hero.run;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_R) {
-                    current = jump;
+                    current = hero.jump;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_T) {
-                    current = mid;
+                    current = hero.mid;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_Y) {
-                    current = fall;
+                    current = hero.fall;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_U) {
-                    current = slide;
+                    current = hero.slide;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_I) {
-                    current = grab;
+                    current = hero.grab;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_O) {
-                    current = climb;
+                    current = hero.climb;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_P) {
-                    current = idle2;
+                    current = hero.idle2;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_A) {
-                    current = attack1;
+                    current = hero.attack1;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_S) {
-                    current = attack2;
+                    current = hero.attack2;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_D) {
-                    current = attack3;
+                    current = hero.attack3;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_F) {
-                    current = hurt;
+                    current = hero.hurt;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_G) {
-                    current = die;
+                    current = hero.die;
                 }
                 else if (event.key.keysym.scancode == SDL_SCANCODE_H) {
-                    current = jump;
+                    current = hero.jump;
                 }
                 index = 0;
             }
@@ -148,11 +128,11 @@ int main(int argc, char* args[])
             window.render(e);
 
         std::pair<size_t, size_t> currentPair = current[index];
-        size_t position = currentPair.second + currentPair.first * nbCol;
+        size_t position = currentPair.second + currentPair.first * hero._col;
         SDL_Rect * dstrect;
         SDL_Rect dst = {256, 394, 120, 120};
         dstrect = &dst;
-        SDL_RenderCopy(window.getRenderer(), hero, rects[position], dstrect);
+        SDL_RenderCopy(window.getRenderer(), heroTexture, rects[position], dstrect);
 
         /*Updates the screen*/
         SDL_RenderPresent(window.getRenderer());
@@ -191,7 +171,7 @@ int main(int argc, char* args[])
     window.cleanUp();
     for(auto e: entitiees)
         SDL_DestroyTexture(e.getText());
-    SDL_DestroyTexture(hero);
+    SDL_DestroyTexture(heroTexture);
     SDL_DestroyRenderer(window.getRenderer());
     SDL_Quit();
 
