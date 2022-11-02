09efe4e98a9031f346b59e813518672dd80fcf3b
alex13dumi <alex13.dumi@gmail.com>
2022-11-02 20:25:13 +0200

    method addRects creates memory space for hero's sprites

--- src/sprites.cpp
+++ src/sprites.cpp
@@ -1,3 +1,10 @@
 #include <Sprites.hpp>
 
 Hero::Hero(std::size_t row, std::size_t col, std::size_t widht, std::size_t height): _col(col), _row(row), _width(widht), _height(height){ };
+void Hero::addRects() {
+    for (std::size_t i = 0; i < _row; i++) {
+        for (std::size_t j = 0; j < _col; j++) {
+            rects.push_back(new SDL_Rect{ (int) (j*_width), (int) (i * _height), (int) _width, (int) _height });
+        }
+    }
+}
